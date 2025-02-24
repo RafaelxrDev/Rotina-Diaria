3aaa9714e50d3c3231e386ffc2b473065607e398
Rafael Xavier Rezende <1255050@sga.pucminas.br>
2025-02-24 16:48:46 -0300

    Adicionei o Controller pela metade

--- Models/Compromissos.cs
+++ Models/Compromissos.cs
@@ -6,7 +6,7 @@ namespace RotinaDiaria.Models
         public string Nome { get;set; } //Nome do COmpromisso (ex: Treino de pernas)
         public string Tipo { get;set; } // Tipo: "Academia" "Refeição" " Estudo" 
         public string Hora { get;set; } // Horario (07:00)
-        public int Duraçao { get;set; } //Duração em Minutos
+        public int Duracao { get;set; } //Duração em Minutos
       
     }
 }
--- obj/Debug/net9.0/RotinaDiaria.AssemblyInfo.cs
+++ obj/Debug/net9.0/RotinaDiaria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RotinaDiaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86abc632a88cf2a023fa2a78c89faf2eb2d3f119")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+887f378efb6c166ba8a791c16e7811e7b081ce79")]
 [assembly: System.Reflection.AssemblyProductAttribute("RotinaDiaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RotinaDiaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
