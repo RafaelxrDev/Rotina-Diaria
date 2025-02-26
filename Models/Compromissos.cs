886cdb199bd4a7805bbecc24b049d896db7a19b0
Rafaelxr <rafael.xr98@gmail.com>
2025-02-25 21:44:22 -0300

    Swagger funcionando 100%

--- Models/Compromissos.cs
+++ Models/Compromissos.cs
@@ -2,11 +2,11 @@ namespace RotinaDiaria.Models
 {
     public class Compromisso
     {
-        public int Id { get;set; } //Identificador Unico do compromisso
-        public string Nome { get;set; } //Nome do COmpromisso (ex: Treino de pernas)
-        public string Tipo { get;set; } // Tipo: "Academia" "Refeição" " Estudo" 
-        public string Hora { get;set; } // Horario (07:00)
-        public int Duracao { get;set; } //Duração em Minutos
+            public int Id { get;set; } //Identificador Unico do compromisso
+            public string Nome { get; set; } = string.Empty;
+            public string Tipo { get; set; } = string.Empty;
+            public string Hora { get; set; } = string.Empty;
+            public int Duracao { get;set; } //Duração em Minutos
       
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
