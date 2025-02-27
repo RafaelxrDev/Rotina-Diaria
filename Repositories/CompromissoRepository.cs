b0761283c675865637e4f71359931764258b40f5
Rafael Xavier Rezende <1255050@sga.pucminas.br>
2025-02-27 14:01:28 -0300

    Feito o async

--- Repositories/CompromissoRepository.cs
+++ Repositories/CompromissoRepository.cs
@@ -1,4 +1,43 @@
 using MongoDB.Driver;
 using RotinaDiaria.Models;
-using RotinaDiaria.config;
+using RotinaDiaria.Config;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
+namespace RotinaDiaria.Repository
+{
+    public class CompromissoRepository
+    {
+        private readonly IMongoCollection<Compromisso> _compromissos;
+
+        public CompromissoRepository(MongoDBConfig dbConfig)
+        {
+            _compromissos = dbConfig.GetCollection<Compromisso>("Compromissos");
+        }
+
+        public async Task<List<Compromisso>> GetAllAsync()
+        {
+            return await _compromissos.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<Compromisso> GetByIdAsync(string id)
+        {
+            return await _compromissos.Find(c => c.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateAsync(Compromisso compromisso)
+        {
+            await _compromissos.InsertOneAsync(compromisso);
+        }
+
+        public async Task UpdateAsync(string id, Compromisso compromissoAtualizado)
+        {
+            await _compromissos.ReplaceOneAsync(c => c.Id == id, compromissoAtualizado);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            await _compromissos.DeleteOneAsync(c => c.Id == id);
+        }
+    }
+}
